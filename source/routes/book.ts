32adc203ab2202a6e5c339a2c292528ee8d48080
bolatah <bolatah@outlook.com>
2022-04-21 13:48:08 +0200

    first commit

---
+++ source/routes/book.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import controller from "../controllers/book";
+const router = express.Router();
+
+router.get("/get/books", controller.getAllBooks);
+router.post("/create/book", controller.createBook);
+export = router;
